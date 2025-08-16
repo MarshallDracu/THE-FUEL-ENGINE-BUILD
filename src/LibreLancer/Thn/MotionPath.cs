e16f8ddbf53a5964413c0c247644d2ddde376800
MarshallDracu <dracularpg@gmail.com>
2025-08-16 11:54:12 +0100

    Update MotionPath.cs

--- src/LibreLancer/Thn/MotionPath.cs
+++ src/LibreLancer/Thn/MotionPath.cs
@@ -135,38 +135,37 @@ if (curve) {
                     lengthPercents[i] = segmentLengths[i] / totalLength;
             }
         }
-public Vector3 GetPosition(float t)
-{
-    if (t >= 1) return endPoint;
-    if (t <= 0) return startPoint;
-
-    if (!curve)
-    {
-        float dist = Vector3.Distance(startPoint, endPoint);
-        var direction = (endPoint - startPoint).Normalized();
-        return startPoint + (direction * (dist * t));
-    }
-    else
-    {
-        float seg0 = 0;
-        for (int i = 0; i < segments.Length; i++)
+        public Vector3 GetPosition(float t)
         {
-            float segLen = lengthPercents[i];
-            float seg1 = seg0 + segLen;
+            if (t >= 1) return endPoint;
+            if (t <= 0) return startPoint;
 
-            if (t <= seg1)
+            if (!curve)
             {
-                var denom = (segLen <= 0 ? 1e-6f : segLen);
-                var t2 = (t - seg0) / denom;   // t2 w [0..1] w obrębie segmentu
-                return segments[i].ValueAt(t2);
+                float dist = Vector3.Distance(startPoint, endPoint);
+                var direction = (endPoint - startPoint).Normalized();
+                return startPoint + (direction * (dist * t));
             }
+            else
+            {
+                float seg0 = 0;
+                for (int i = 0; i < segments.Length; i++)
+                {
+                    float segLen = lengthPercents[i];
+                    float seg1 = seg0 + segLen;
 
-            seg0 = seg1;
+                    if (t <= seg1)
+                    {
+                        var denom = (segLen <= 0 ? 1e-6f : segLen);
+                        var t2 = (t - seg0) / denom; // [0..1] w obrębie segmentu
+                        return segments[i].ValueAt(t2);
+                    }
+
+                    seg0 = seg1;
+                }
+                throw new Exception("Something has gone horribly wrong in MotionPath");
+            }
         }
-        throw new Exception("Something has gone horribly wrong in MotionPath");
-    }
-}
-
 
         public Vector3 GetDirection(float t, bool reverse = false)
         {
